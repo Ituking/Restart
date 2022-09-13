02754696cf657bac35645ae398abab4f7485be04
Ituking <77528963+Ituking@users.noreply.github.com>
2022-09-14 06:27:52 +0900

    Swift進捗
    Restartアプリの作成

---
+++ Restart/Screen/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  Restart
+//
+//  Created by 大久保樹 on 2022/09/14.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
---
+++ Restart/Screen/OnboardingView.swift
@@ -0,0 +1,20 @@
+//
+//  OnboardingView.swift
+//  Restart
+//
+//  Created by 大久保樹 on 2022/09/14.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView()
+    }
+}
