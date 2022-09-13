11b864e95ae281f8c4a4ced2a3ac22099ab9d5e6
Ituking <77528963+Ituking@users.noreply.github.com>
2022-09-14 01:43:07 +0900

    Initial commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by 大久保樹 on 2022/09/14.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
